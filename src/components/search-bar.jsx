4ea0073f5cccbdd910917152b1d6f0fb5502f0be
Aayush Rai <aayushrai0911@gmail.com>
2024-12-03 20:11:07 +0530

    Basic search url params implementation

--- src/components/search-bar.jsx
+++ src/components/search-bar.jsx
@@ -1,9 +1,10 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import {STOPS} from '../utils/constants.js';
 import {getCurrentLocation, getDistance} from '../utils';
 import navigationArrow from '../assets/images/navigation-arrow-duotone.svg';
 import magnifyingGlass from '../assets/images/magnifying-glass.svg';
+import copy from '../assets/images/copy-simple.svg';
 function SearchBar({setSearchResults}) {
     const locationThreshold = 0.2; // 200 meters
     const [searchValue, setSearchValue] = useState('');
@@ -11,7 +12,7 @@ function SearchBar({setSearchResults}) {
     const [positionToggled, setPositionToggled] = useState(false);
     // Handle search term change and update items
     const handleSearchChange = (e) => {
-        const searchTerm = e.target ? e.target.value : e.value;
+        const searchTerm = e.target ? e.target.value : (e.value ? e.value : e);
         const newSearchTerm = searchTerm ? searchTerm : '';
         setSearchValue(newSearchTerm);
         const searchFilter = newSearchTerm.length > 2 ? newSearchTerm : '';
@@ -30,8 +31,6 @@ function SearchBar({setSearchResults}) {
                 return false;
             }
             const distance = getDistance(platform.coords[1], platform.coords[0], coords.latitude, coords.longitude);
-            console.log('DISTANCE');
-            console.log(distance);
             if(actualThreshold >= distance) {
                 return true;
             }
@@ -44,14 +43,12 @@ function SearchBar({setSearchResults}) {
             return false;
         }
         const distance = getDistance(coords.latitude, coords.longitude, stopData.stop_lat, stopData.stop_lon);
-        // console.log('DISTANCE');
         // console.log(distance);
         return actualThreshold >= distance;
 
     };
     // Handle search by current location
     const handleLocationSearch =  () => {
-        // console.log('inside location search');
         // Placeholder logic for location-based search
         if(positionToggled) {
             setCurrentPosition(null);
@@ -67,35 +64,45 @@ function SearchBar({setSearchResults}) {
         const items =
             Object.entries(STOPS).reduce((acc, [key, value]) =>
                 {
-                    console.log(acc);
                     if (!Array.isArray(value)) {
                         console.error(`Expected 'value' to be an array for key ${key}, but got:`, value);
                         return acc; // Skip if value is not an array
                     }
                     value.forEach((item) => {
-                        // console.log(`ITEM ${key}`);
                         if(acc.includes(key)){
                             return acc;
                         }
                         if(item.platforms) {
                             if(handlePlatformsLocations(item.platforms, coords)) {
-                                console.log(`PUSHING ${key}`);
                                 acc.push(key);
                             }
                         }
                         if(handleStopLocation(item, coords)) {
-                            console.log(`PUSHING ${key}`);
                             acc.push(key);
                         }
                     });
                     return acc;
                 }, []
             );
-        console.log(`ITEMS`);
-        console.log(items);
         setPositionToggled(true);
         setSearchResults(items ? items : []);
     }
+
+    useEffect(() => {
+
+        const urlParams = new URLSearchParams(window.location.search);
+        if(urlParams.has('search')) {
+            setSearchValue(urlParams.get('search'));
+            handleSearchChange(urlParams.get('search'));
+        }
+        if(urlParams.has('lat') && urlParams.has('lon') && urlParams.has('acc')) {
+            setCurrentPosition({coords: {
+                    latitude: urlParams.get('lat'),
+                    longitude: urlParams.get('lon'),
+                    accuracy: urlParams.get('acc')
+                }});
+        }
+    }, []);
 
     return (
         <div className="search-bar">
@@ -111,6 +118,22 @@ function SearchBar({setSearchResults}) {
                 onChange={handleSearchChange}
                 className="search-input"
             />
+            <button className="location-button" onClick={() => {
+                const url = `${window.location.protocol}//` +
+                    `${window.location.host}/?search=${searchValue}${positionToggled ?
+                        `&lat=${currentPosition.coords.latitude}&lon=` +
+                        `${currentPosition.coords.longitude}&acc=${currentPosition.coords.accuracy}` : ``}`;
+                navigator.clipboard.writeText(url)
+                    .then(() =>
+                        window.open(url, '_self')
+                    );
+            }}>
+                <img
+                    src={copy}
+                    alt="Copy/Refresh"
+                    className="icon24"
+                />
+            </button>
             <button onClick={handleLocationSearch} className="location-button">
                 <img
                     src={navigationArrow}
